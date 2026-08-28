8f415616cc9223f5345cc6eb145cc7e5ac9777d0
kigawa <mail@kigawa.net>
2026-08-28 17:24:02 +0900

    Initialize Kotlin Multiplatform project with native targets and IntelliJ IDEA configuration files

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("multiplatform") version "2.0.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    macosX64("macos") {
+        binaries {
+            executable()
+        }
+    }
+    linuxX64("linux") {
+        binaries {
+            executable()
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting
+        val nativeMain by creating {
+            dependsOn(commonMain)
+            kotlin.srcDirs("src/nativeMain/kotlin")
+        }
+        val macosMain by getting {
+            dependsOn(nativeMain)
+        }
+        val linuxMain by getting {
+            dependsOn(nativeMain)
+        }
+    }
+}
