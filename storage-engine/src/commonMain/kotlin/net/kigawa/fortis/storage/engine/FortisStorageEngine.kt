915b364e8b3523577b85d737c7390d7a9d2e5119
kigawa <mail@kigawa.net>
2026-08-28 18:02:21 +0900

    Add artifact configuration and CRUD stubs to `FortisStorageEngine` class

--- storage-engine/src/commonMain/kotlin/net/kigawa/fortis/storage/engine/FortisStorageEngine.kt
+++ storage-engine/src/commonMain/kotlin/net/kigawa/fortis/storage/engine/FortisStorageEngine.kt
@@ -2,4 +2,15 @@ package net.kigawa.fortis.storage.engine
 
 @Suppress("unused")
 class FortisStorageEngine {
+    suspend fun get(key: ByteArray): ByteArray? {
+        TODO()
+    }
+
+    suspend fun put(key: ByteArray, value: ByteArray) {
+        TODO()
+    }
+
+    suspend fun delete(key: ByteArray): Boolean {
+        TODO()
+    }
 }
