ef572cb23f81730cce1b6275340801931b22c9c1
kigawa <mail@kigawa.net>
2026-08-31 13:52:33 +0900

    Update `FsOutput`: add support for partial writes with `dataOffset` and `length` parameters

--- io/src/commonMain/kotlin/net/kigawa/fortis/io/fs/FsOutput.kt
+++ io/src/commonMain/kotlin/net/kigawa/fortis/io/fs/FsOutput.kt
@@ -5,8 +5,18 @@ import net.kigawa.fortis.io.Output
 interface FsOutput: Output {
     val file: FortisFile
 
-    suspend fun writeAt(offset: FsOffset, data: ByteArray): Int
-    suspend fun writeAppend(data: ByteArray): Int
+    suspend fun writeAt(
+        offset: FsOffset, data: ByteArray,
+        dataOffset: Int,
+        length: Int,
+    ): Int
+
+    suspend fun writeAppend(
+        data: ByteArray,
+        dataOffset: Int,
+        length: Int,
+    ): Int
+
     suspend fun sync()
     suspend fun truncate(size: FsByteSize)
 
--- io/src/jvmMain/kotlin/net/kigawa/fortis/io/fs/JvmFsOutput.kt
+++ io/src/jvmMain/kotlin/net/kigawa/fortis/io/fs/JvmFsOutput.kt
@@ -9,12 +9,18 @@ data class JvmFsOutput(
     override val file: FortisFile,
     val channel: FileChannel,
 ): FsOutput {
-    override suspend fun writeAt(offset: FsOffset, data: ByteArray): Int = withContext(Dispatchers.IO) {
-        channel.write(ByteBuffer.wrap(data), offset)
+    override suspend fun writeAt(
+        offset: FsOffset, data: ByteArray,
+        dataOffset: Int,
+        length: Int,
+    ): Int = withContext(Dispatchers.IO) {
+        channel.write(ByteBuffer.wrap(data, dataOffset, length), offset)
     }
 
-    override suspend fun writeAppend(data: ByteArray): Int = withContext(Dispatchers.IO) {
-        writeAt(channel.size(), data)
+    override suspend fun writeAppend(
+        data: ByteArray, dataOffset: Int, length: Int,
+    ): Int = withContext(Dispatchers.IO) {
+        writeAt(channel.size(), data, dataOffset, length)
     }
 
     override suspend fun sync() = withContext(Dispatchers.IO) {
