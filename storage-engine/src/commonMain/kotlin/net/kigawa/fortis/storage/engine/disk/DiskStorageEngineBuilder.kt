15ec6158112de916a25ae3c48db2d607a0d26dc2
kigawa <mail@kigawa.net>
2026-09-17 04:21:53 +0900

    Make `DiskStorageEngineBuilder` internal for restricted access

--- storage-engine/src/commonMain/kotlin/net/kigawa/fortis/storage/engine/disk/DiskStorageEngineBuilder.kt
+++ storage-engine/src/commonMain/kotlin/net/kigawa/fortis/storage/engine/disk/DiskStorageEngineBuilder.kt
@@ -4,7 +4,7 @@ import net.kigawa.fortis.storage.engine.memory.MemoryStorageEngine
 import net.kigawa.fortis.storage.engine.wal.Wal
 import net.kigawa.fortis.storage.engine.wal.WalOperation
 
-class DiskStorageEngineBuilder {
+internal class DiskStorageEngineBuilder {
     private var wal: Wal? = null
     private var memory: MemoryStorageEngine? = null
 
